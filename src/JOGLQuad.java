696d5ec492d1fcaabc2711c8b212805be346e0af
Christian Fransson <st_anger01@hotmail.com>
2012-01-17 19:45:34 +0100

    added mouse support

--- src/JOGLQuad.java
+++ src/JOGLQuad.java
@@ -1,10 +1,14 @@
+import java.awt.AWTException;
 import java.awt.Component;
 import java.awt.Frame;
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
- 
+import java.awt.image.BufferedImage;
+import java.awt.Robot;	//used to set mouse cursor on the same coordinates as quad
 import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
 import javax.media.opengl.GL2ES1;
@@ -14,16 +18,17 @@ import javax.media.opengl.awt.GLCanvas;
 import javax.media.opengl.fixedfunc.GLLightingFunc;
 import javax.media.opengl.fixedfunc.GLMatrixFunc;
 import javax.media.opengl.glu.GLU;
+import javax.swing.event.MouseInputListener;
+import wiiusej.*;
  
 import com.jogamp.opengl.util.Animator;
  
  
-public class JOGLQuad implements GLEventListener, KeyListener {
+public class JOGLQuad implements GLEventListener, KeyListener, MouseInputListener{
     float centerX=(float)0.5;    
     float centerY=(float)0.5; 
     float deltaY=(float)0.1;
     float deltaX=(float)0.1;
-    
     float rotateT = 0.0f;
  
     static GLU glu = new GLU();
@@ -33,7 +38,9 @@ public class JOGLQuad implements GLEventListener, KeyListener {
     static Frame frame = new Frame("Jogl Quad drawing");
  
     static Animator animator = new Animator(canvas);
- 
+    
+    //Wiimote[] wiimote = WiiUseApiManager.getWiimotes(1, true);
+    
     public void display(GLAutoDrawable gLDrawable) {
         final GL2 gl = gLDrawable.getGL().getGL2();
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);
@@ -73,6 +80,7 @@ public class JOGLQuad implements GLEventListener, KeyListener {
     }
  
     public void init(GLAutoDrawable gLDrawable) {
+    	//centerMouse();
         GL2 gl = gLDrawable.getGL().getGL2();
         gl.glShadeModel(GLLightingFunc.GL_SMOOTH);
         gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -80,7 +88,11 @@ public class JOGLQuad implements GLEventListener, KeyListener {
         gl.glEnable(GL.GL_DEPTH_TEST);
         gl.glDepthFunc(GL.GL_LEQUAL);
         gl.glHint(GL2ES1.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST);
+        //add listeners, 
         ((Component) gLDrawable).addKeyListener(this);
+        ((Component) gLDrawable).addMouseMotionListener(this);
+        ((Component) gLDrawable).addMouseListener(this);   
+        //wiimote[0].addWiiMoteEventListeners(new wiimoteListener());
     }
  
     public void reshape(GLAutoDrawable gLDrawable, int x, int y, int width, int height) {
@@ -124,6 +136,7 @@ public class JOGLQuad implements GLEventListener, KeyListener {
     }
  
     public static void main(String[] args) {
+    	
         canvas.addGLEventListener(new JOGLQuad());
         frame.add(canvas);
         frame.setSize(640, 480);
@@ -137,9 +150,78 @@ public class JOGLQuad implements GLEventListener, KeyListener {
         frame.setVisible(true);
         animator.start();
         canvas.requestFocus();
+        //hide cursor
+        frame.setCursor(frame.getToolkit().createCustomCursor(
+                new BufferedImage(3, 3, BufferedImage.TYPE_INT_ARGB), new Point(0, 0),
+                "null"));
     }
  
     public void dispose(GLAutoDrawable gLDrawable) {
         // do nothing
     }
+    
+    //unused method
+	public void centerMouse() {
+		//this code works if you eg. put it in a event listener method, but moves quad with it
+	//if(frame.isShowing()){
+	    Point locOnScreen = frame.getLocationOnScreen();
+	    System.out.println("mouse location");
+	    System.out.println(locOnScreen.x + (frame.getWidth() / 2));
+	    System.out.println(locOnScreen.y + (frame.getWidth() / 2));
+	    int middleX = locOnScreen.x + (frame.getWidth() / 2);
+	    int middleY = locOnScreen.y + (frame.getHeight() / 2);
+	    try{
+	      Robot rob = new Robot();
+	      rob.mouseMove(middleX, middleY);
+	    }catch(Exception e){System.out.println(e);}
+		    //setting mouse coords
+		//  }
+	}
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		//System.out.println(arg0.getXOnScreen());
+		float offset =4; //used to alleviate the consequences of the cursor being too far away from the quad at startup
+		centerY=-((float)arg0.getYOnScreen() / 100)+offset;
+		centerX=((float)arg0.getXOnScreen() / 100)-offset-1;
+		//r.delay(500);	fun effect to make game harder if using a robot object
+		
+		
+	}
 }
