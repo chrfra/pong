3ad0b96701581fdb992952eea70909d9e492a029
Johan Sandstrom <johan.m.sandstrom@gmail.com>
2012-02-01 19:17:55 +0100

    Wrote method that draws a 3D rectangle with right coords and dimension.

--- src/pong/view/GraphicsEngine.java
+++ src/pong/view/GraphicsEngine.java
@@ -9,6 +9,7 @@ import java.awt.Component;
 import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.InvalidClassException;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
@@ -30,7 +31,7 @@ public class GraphicsEngine implements GLEventListener {
 	private static GLU glu = new GLU();
 	private static GLCanvas canvas = new GLCanvas();
 	private static Frame frame = new Frame("Pong");
-
+	private int rotation = 0;
 	private GameEngine ge;
 
 	private MouseInput mouse;
@@ -61,9 +62,28 @@ public class GraphicsEngine implements GLEventListener {
 
 	@Override
 	public void display(GLAutoDrawable gLDrawable) {
-		// Draw walls around playarea
+		GL2 gl = gLDrawable.getGL().getGL2();
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+		gl.glClear(GL.GL_DEPTH_BUFFER_BIT);
+		gl.glLoadIdentity();
+
+		rotation += 1;
+
+		//TODO Draw walls around playarea
+		
 		// Draw paddles, ball etc
-		this.drawPaddle(gLDrawable, null);
+		try {
+			// IMPORTANT! PopMatrix() resets glTranslatef and glRotatef to what it was before the previous PushMatrix()
+			gl.glPushMatrix();
+			this.draw3DRectangle(gl, new Paddle(50, 50, 0, 5, 10, 2));
+			gl.glPopMatrix();
+			gl.glPushMatrix();
+			this.draw3DRectangle(gl, new Paddle(-30, -50, 0, 3, 40, 2));
+			gl.glPopMatrix();
+		} catch (InvalidClassException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
@@ -94,8 +114,7 @@ public class GraphicsEngine implements GLEventListener {
 	}
 
 	@Override
-	public void reshape(GLAutoDrawable gLDrawable, int x, int y, int width,
-			int height) {
+	public void reshape(GLAutoDrawable gLDrawable, int x, int y, int width, int height) {
 		GL2 gl = gLDrawable.getGL().getGL2();
 		if (height <= 0) {
 			height = 1;
@@ -107,7 +126,7 @@ public class GraphicsEngine implements GLEventListener {
 
 		glu.gluPerspective(50.0f, h, 1.0, 1000.0);
 		// Set camera to look at Origo from 20 units away.
-		glu.gluLookAt(0.0, 0.0, 20.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+		glu.gluLookAt(0.0, 0.0, 100.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 		gl.glMatrixMode(GLMatrixFunc.GL_MODELVIEW);
 		gl.glLoadIdentity();
 	}
@@ -118,20 +137,71 @@ public class GraphicsEngine implements GLEventListener {
 		System.exit(0);
 	}
 
-	public void drawPaddle(GLAutoDrawable drawable, Paddle pad) {
-		GL2 gl = drawable.getGL().getGL2();
+	/*
+	 * Renders a 3D rectangle.
+	 * This can be used to draw a paddle or obstacle or something else.
+	 */
+	public void draw3DRectangle(GL2 gl, GameItem item) throws InvalidClassException {
+		// xPos, yPos, zPos, width, height, depth
+		float x, y, z, w, h, d;
 
-		// gl.glPushMatrix();
-		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
-		gl.glClear(GL.GL_DEPTH_BUFFER_BIT);
-		gl.glLoadIdentity();
+		// Check that item is a Paddle. Need to support all classes that have the same shape in the future.
+		if (item instanceof Paddle) {
+			Paddle pad = (Paddle) item;
+			x = pad.getxPos();
+			y = pad.getyPos();
+			z = pad.getzPos();
+			w = pad.getWidth();
+			h = pad.getHeight();
+			d = pad.getDepth();
+		} else {
+			throw new InvalidClassException("Wrong class of GameItem in draw3DRectangle(GL2 gl, GameItem item)");
+		}
 
-		// TODO Draw paddle here. Use the data in the Paddle object to draw it
-		// in the right place.
-		gl.glRotatef(30, 1.0f, 1.0f, 1.0f);
-		gl.glTranslatef(5.0f, -5.0f, -20.0f);
+		// Move to right coordinates.
+		gl.glTranslatef(x / 2f, y / 2f, z / 2f);
+		gl.glRotatef(rotation, 1.0f, 1.0f, 1.0f);
+
 		gl.glBegin(GL2.GL_QUADS); // Draw A Quad
 
+		gl.glColor3f(0.0f, 1.0f, 0.0f); // Set The Color To Green
+		gl.glVertex3f(w / 2f, h / 2f, -d / 2f); // Top Right Of The Quad (Top)
+		gl.glVertex3f(-w / 2f, h / 2f, -d / 2f); // Top Left Of The Quad (Top)
+		gl.glVertex3f(-w / 2f, h / 2f, d / 2f); // Bottom Left Of The Quad (Top)
+		gl.glVertex3f(w / 2f, h / 2f, d / 2f); // Bottom Right Of The Quad (Top)
+
+		gl.glColor3f(1.0f, 0.5f, 0.0f); // Set The Color To Orange
+		gl.glVertex3f(w / 2f, -h / 2f, d / 2f); // Top Right Of The Quad (Bottom)
+		gl.glVertex3f(-w / 2f, -h / 2f, d / 2f); // Top Left Of The Quad (Bottom)
+		gl.glVertex3f(-w / 2f, -h / 2f, -d / 2f); // Bottom Left Of The Quad (Bottom)
+		gl.glVertex3f(w / 2f, -h / 2f, -d / 2f); // Bottom Right Of The Quad (Bottom)
+
+		gl.glColor3f(1.0f, 0.0f, 0.0f); // Set The Color To Red
+		gl.glVertex3f(w / 2, h / 2f, d / 2f); // Top Right Of The Quad (Front)
+		gl.glVertex3f(-w / 2, h / 2f, d / 2f); // Top Left Of The Quad (Front)
+		gl.glVertex3f(-w / 2, -h / 2f, d / 2f); // Bottom Left Of The Quad (Front)
+		gl.glVertex3f(w / 2, -h / 2f, d / 2f); // Bottom Right Of The Quad (Front)
+
+		gl.glColor3f(1.0f, 1.0f, 0.0f); // Set The Color To Yellow
+		gl.glVertex3f(w / 2f, -h / 2f, -d / 2f); // Bottom Left Of The Quad (Back)
+		gl.glVertex3f(-w / 2f, -h / 2f, -d / 2f); // Bottom Right Of The Quad (Back)
+		gl.glVertex3f(-w / 2f, h / 2f, -d / 2f); // Top Right Of The Quad (Back)
+		gl.glVertex3f(w / 2f, h / 2f, -d / 2f); // Top Left Of The Quad (Back)
+
+		gl.glColor3f(0.0f, 0.0f, 1.0f); // Set The Color To Blue
+		gl.glVertex3f(-w / 2f, h / 2f, d / 2f); // Top Right Of The Quad (Left)
+		gl.glVertex3f(-w / 2f, h / 2f, -d / 2f); // Top Left Of The Quad (Left)
+		gl.glVertex3f(-w / 2f, -h / 2f, -d / 2f); // Bottom Left Of The Quad (Left)
+		gl.glVertex3f(-w / 2f, -h / 2f, d / 2f); // Bottom Right Of The Quad (Left)
+
+		gl.glColor3f(1.0f, 0.0f, 1.0f); // Set The Color To Violet
+		gl.glVertex3f(w / 2f, h / 2f, -d / 2f); // Top Right Of The Quad (Right)
+		gl.glVertex3f(w / 2f, h / 2f, d / 2f); // Top Left Of The Quad (Right)
+		gl.glVertex3f(w / 2f, -h / 2f, d / 2f); // Bottom Left Of The Quad (Right)
+		gl.glVertex3f(w / 2f, -h / 2f, -d / 2f); // Bottom Right Of The Quad (Right)
+
+		/*Just draw a cube. No respect for xyz or dimensions
+		/*		
 		gl.glColor3f(0.0f, 1.0f, 0.0f); // Set The Color To Green
 		gl.glVertex3f(1.0f, 1.0f, -1.0f); // Top Right Of The Quad (Top)
 		gl.glVertex3f(-1.0f, 1.0f, -1.0f); // Top Left Of The Quad (Top)
@@ -167,6 +237,8 @@ public class GraphicsEngine implements GLEventListener {
 		gl.glVertex3f(1.0f, 1.0f, 1.0f); // Top Left Of The Quad (Right)
 		gl.glVertex3f(1.0f, -1.0f, 1.0f); // Bottom Left Of The Quad (Right)
 		gl.glVertex3f(1.0f, -1.0f, -1.0f); // Bottom Right Of The Quad (Right)
+		*/
+
 		gl.glEnd(); // Done Drawing The Quad
 	}
 }
